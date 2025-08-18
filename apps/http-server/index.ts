f93c6a7bb6addb3a06d6be17450d28eec4e58613
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-08-18 23:28:32 +0530

    done with all

---
+++ apps/http-server/index.ts
@@ -0,0 +1,23 @@
+import {prisma} from "@repo/database/client"
+import express, { Request, Response } from "express"
+
+const app = express();
+app.use(express.json())
+
+app.get("/",async(req:Request,res:Response)=>{
+   const users  = await prisma.user.findMany();
+   res.send(users)
+})
+app.post("/",async(req:Request,res:Response)=>{
+    console.log(req.body)
+    const users = await prisma.user.create({
+        data:{
+            id : req.body.id,
+            email: req.body.email
+        }
+    })
+})
+app.listen(3000,()=>{
+    console.log("Http-Server Started")
+})
+
